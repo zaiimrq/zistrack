12488e065a4900c8fe89b86b7b39e0089af5473e
zaiimrq <zaiimrq@gmail.com>
2025-11-08 12:26:41 +0900

    fix 403 forbidden error

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,32 +6,36 @@ namespace App\Models;
 use App\Enums\UserRole;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     public $timestamps = false;
 
     protected $fillable = [
-        'name',
-        'email',
-        'password',
-        'role',
-        'google_id',
-        'target',
+        "name",
+        "email",
+        "password",
+        "role",
+        "google_id",
+        "target",
     ];
 
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    protected $hidden = ["password", "remember_token"];
 
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-            'role' => UserRole::class,
+            "email_verified_at" => "datetime",
+            "password" => "hashed",
+            "role" => UserRole::class,
         ];
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
     }
 
     public function isAdmin(): bool
