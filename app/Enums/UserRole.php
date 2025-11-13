d6613c68647da997eccc615674fdb81eaae0bb29
zaiimrq <zaiimrq@gmail.com>
2025-11-14 05:49:47 +0900

    add 'lead' role

--- app/Enums/UserRole.php
+++ app/Enums/UserRole.php
@@ -2,18 +2,30 @@
 
 namespace App\Enums;
 
+use Filament\Support\Contracts\HasColor;
 use Filament\Support\Contracts\HasLabel;
 
-enum UserRole: string implements HasLabel
+enum UserRole: string implements HasLabel, HasColor
 {
-    case ADMIN = 'admin';
-    case USER = 'user';
+    case ADMIN = "admin";
+    case USER = "user";
+    case LEAD = "lead";
 
     public function getLabel(): string
     {
         return match ($this) {
-            UserRole::ADMIN => 'Admin',
-            UserRole::USER => 'User',
+            UserRole::ADMIN => "Admin",
+            UserRole::USER => "User",
+            UserRole::LEAD => "Lead",
+        };
+    }
+
+    public function getColor(): string|array|null
+    {
+        return match ($this) {
+            UserRole::ADMIN => "primary",
+            UserRole::LEAD => "success",
+            UserRole::USER => "warning",
         };
     }
 }
