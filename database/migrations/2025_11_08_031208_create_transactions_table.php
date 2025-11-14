eeb417d75c8726a3880131b542ff24977a08d94c
zaiimrq <zaiimrq@gmail.com>
2025-11-14 11:05:40 +0900

    fix constraint

--- database/migrations/2025_11_08_031208_create_transactions_table.php
+++ database/migrations/2025_11_08_031208_create_transactions_table.php
@@ -4,19 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('transactions', function (Blueprint $table) {
+        Schema::create("transactions", function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('donatur_id')->constrained();
-            $table->integer('amount', 0);
-            $table->string('proof_file', 255);
+            $table->foreignId("user_id")->constrained()->cascadeOnDelete();
+            $table->foreignId("donatur_id")->constrained()->cascadeOnDelete();
+            $table->integer("amount", 0);
+            $table->string("proof_file", 255);
             $table->timestamps();
         });
     }
@@ -26,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('transactions');
+        Schema::dropIfExists("transactions");
     }
 };
