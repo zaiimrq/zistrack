019e9682fb00b2f7adf4f852b4fdcaba43a3be3d
zaiimrq <zaiimrq@gmail.com>
2025-11-22 20:14:51 +0900

    ci

--- app/Filament/Resources/Transactions/TransactionResource.php
+++ app/Filament/Resources/Transactions/TransactionResource.php
@@ -35,16 +35,16 @@ class TransactionResource extends Resource
     public static function getRelations(): array
     {
         return [
-                //
-            ];
+            //
+        ];
     }
 
     public static function getPages(): array
     {
         return [
-            "index" => ListTransactions::route("/"),
-            "create" => CreateTransaction::route("/create"),
-            "edit" => EditTransaction::route("/{record}/edit"),
+            'index' => ListTransactions::route('/'),
+            'create' => CreateTransaction::route('/create'),
+            'edit' => EditTransaction::route('/{record}/edit'),
         ];
     }
 
@@ -52,18 +52,16 @@ class TransactionResource extends Resource
     {
         $user = filament()->auth()->user();
 
-        return static::getModel()
-            ::query()
+        return static::getModel()::query()
             ->when(
                 $user->isUser(),
-                fn($query): Builder => $query->where("user_id", $user->id),
+                fn ($query): Builder => $query->where('user_id', $user->id),
             );
     }
 
     public static function getNavigationBadge(): ?string
     {
-        $pendingCount = static::getModel()
-            ::query()
+        $pendingCount = static::getModel()::query()
             ->whereStatus(TransactionStatus::Pending)
             ->count();
 
@@ -72,6 +70,6 @@ class TransactionResource extends Resource
 
     public static function getNavigationBadgeColor(): string|array|null
     {
-        return "warning";
+        return 'warning';
     }
 }
--- database/migrations/2025_10_10_112027_create_donaturs_table.php
+++ database/migrations/2025_10_10_112027_create_donaturs_table.php
@@ -4,18 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create("donaturs", function (Blueprint $table) {
+        Schema::create('donaturs', function (Blueprint $table) {
             $table->id();
-            $table->foreignId("user_id")->constrained()->cascadeOnDelete();
-            $table->string("name");
-            $table->integer("phone")->nullable();
-            $table->string("address", 255);
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('name');
+            $table->integer('phone')->nullable();
+            $table->string('address', 255);
         });
     }
 
@@ -24,6 +25,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists("donaturs");
+        Schema::dropIfExists('donaturs');
     }
 };
